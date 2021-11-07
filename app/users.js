8294eee051528d5151dc0b7a2de16995aac8fb9c
Vitaliy Ten <vitalten10@gmail.com>
2021-11-08 00:40:58 +0600

    Создан ввод и айтификация пользователя

---
+++ app/users.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+
+});
+
+router.post('/', async (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send({error: 'Data No Valid'});
+    }
+
+    const userData = {
+        username: req.body.username,
+        password: req.body.password,
+    }
+
+    const user = new User(userData);
+
+    try {
+        user.generateToken();
+        await user.save();
+        res.send(user);
+    } catch (e) {
+        console.log('Have a error');
+        res.status(500).send(e);
+    }
+});
+
+router.post('/sessions', async (req, res) => {
+    const user = await User.findOne({username: req.body.username});
+
+    if (!user) {
+        return res.status(401).send({error: "Username not found"});
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+        return res.status(401).send({error: "Password is wrong"})
+    }
+
+    user.generateToken();
+    await user.save();
+    res.send({message: "Username and password correct", user});
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const exitHook = require('async-exit-hook');
+const users = require('./app/users');
+const tasks = require('./app/tasks');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,9 @@ app.use(cors());
 app.use(express.static('public'));
 
 const port = 8000;
+
+app.use('/users', users);
+app.use('/tasks', tasks);
 
 const run = async () => {
     await mongoose.connect('mongodb://localhost/HW84');
